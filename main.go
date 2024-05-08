68569e19c80014280d79e0066e6d8bb69d5e9ae8
Roman Kislov <roki@boozt.com>
2024-05-08 09:00:09 +0200

    Parse css and js files

--- main.go
+++ main.go
@@ -8,19 +8,21 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 	startURL := "https://books.toscrape.com/"
-
+	start := time.Now()
 	visited := make(map[string]bool)
 	urls := make([]string, 0)
 	elementMatcher := map[string]string{
 		"a":   "href",
 		"img": "src",
 	}
+	urls = parseCssAndJsFiles(startURL)
 
 	var visit func(string)
 	visit = func(u string) {
@@ -64,7 +66,44 @@ func main() {
 	for i, u := range urls {
 		fmt.Println(i, u)
 	}
+	duration := time.Since(start)
+	fmt.Println("Scraping time: ", duration.Seconds(), " sec")
 	fmt.Println("Visited ", len(urls), len(visited), "pages")
+}
+
+func parseCssAndJsFiles(u string) []string {
+	var urls []string
+	elementMatcher := map[string]string{
+		"link":   "href",
+		"script": "src",
+	}
+
+	resp, err := http.Get(u)
+	if err != nil {
+		fmt.Println(err)
+		return urls
+	}
+	defer resp.Body.Close()
+
+	// Create a goquery document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return urls
+	}
+
+	// Parse all CSS and script files
+	for selector, attr := range elementMatcher {
+		doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+			css, exists := s.Attr(attr)
+			if exists {
+				link := resolveLink(u, css)
+				urls = append(urls, link)
+			}
+		})
+	}
+
+	return urls
 }
 
 func resolveLink(baseURL, href string) string {
